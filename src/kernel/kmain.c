91381d94f0cc97d5974ce64a62964746b2c041a5
duckhawk124 <duckhawk124@gmail.com>
2020-05-16 14:47:18 +0900

    add kernel code

---
+++ src/kernel/kmain.c
@@ -0,0 +1,8 @@
+void kmain(){
+  /* print enter kernel message */
+  unsigned long *vga_ptr = (unsigned long*)0xb8000;
+  *vga_ptr = 0x2f4e2f522f452f4b; /* KERN */
+  while(1){
+    /* loop */
+  }
+}
